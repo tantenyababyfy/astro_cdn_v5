7b7469a753a407e48f8a8484545e4d95d7b6cc7c
holicholigan <ekorduarubah@gmail.com>
2024-02-24 09:03:36 +0700

    first commit

---
+++ js/main-v5.js
@@ -0,0 +1,12 @@
+/*!
+ * Item: ASTROPAGE
+ * Description: Homepage Website
+ * Author/Developer: ASTRO
+ * Author/Developer URL: https://linktr.ee/astronimeid
+ * Version: v5
+ * License: Standard Licenses
+ */
+
+/*----------- CUSTOM JS SCRIPTS -----------*/
+
+!function(n){"use strict";n(function(){}),n(window).on("load",function(){})}(jQuery);
